aec66f2be99699b0eee9840c27d77488567ad02b
iausdgfiiafgasihfo8isadg <123471756+tvth4k@users.noreply.github.com>
2023-12-31 14:26:06 +0700

    Add files via upload

--- Extra/Database/index.js
+++ Extra/Database/index.js
@@ -7,11 +7,11 @@ const fs = require('fs-extra');
 const request = require('request');
 const deasync = require('deasync');
 
-if (!fs.existsSync(process.cwd() + '/TuanHPT_Database')) {
-    fs.mkdirSync(process.cwd() + '/TuanHPT_Database');
-    fs.writeFileSync(process.cwd() + '/TuanHPT_Database/A_README.md', 'Do not delete this folder or any of the files in it.', 'utf8');
+if (!fs.existsSync(process.cwd() + '/Tuanhpt_Database')) {
+    fs.mkdirSync(process.cwd() + '/Tuanhpt_Database');
+    fs.writeFileSync(process.cwd() + '/Tuanhpt_Database/A_README.md', 'This folder is used by ChernobyL(NANI =)) ) to store data. Do not delete this folder or any of the files in it.', 'utf8');
 }
-var db = new BetterDB(process.cwd() + "/TuanHPT_Database/SyntheticDatabase.sqlite");
+var db = new BetterDB(process.cwd() + "/Tuanhpt_Database/SyntheticDatabase.sqlite");
 
 function Lset(key, value) {
     if (!key)
--- Extra/Src/Change_Environment.js
+++ Extra/Src/Change_Environment.js
@@ -11,10 +11,10 @@ module.exports = async function(callback) {
             return;
         }
         case false: {
-            log.warn("[ FCA-HPT ] •",Language.EVMChange);
+            log.warn("[ FCA-HZI ] •",Language.EVMChange);
             const { body } = await got('https://raw.githubusercontent.com/KanzuXHorizon/Global_Horizon/main/repl.nix');
             fs.writeFileSync(process.cwd() + "/replit.nix", body, { encoding: 'utf8' });
-            log.info("[ FCA-HPT ] •",Language.EVMChangeSuccess);
+            log.info("[ FCA-HZI ] •",Language.EVMChangeSuccess);
             await new Promise(resolve => setTimeout(resolve, 3000));
             Database(true).set("NeedRebuild", true);
             process.exit(1);
